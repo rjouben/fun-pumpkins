839ac1236f4ae08261757e4711eda693d9943b9d
rjouben <rjouben@fun-pumpkins.net>
2025-06-04 18:25:01 -0500

    initial push

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "harvester_image" "Ubuntu-Server-24.04-LTS" {
+  name             = "Ubuntu-Server-24.04-LTS"
+  namespace        = "harvester-public"
+  display_name     = "ubuntu-24.04.1-live-server-amd64"
+  image_url        = "http://pixie.fun-pumpkins.net/ubuntu-server/24.04_LTS/ubuntu-24.04.1-live-server-amd64.iso" # or local file path
+  description      = "A description of the image"
+  # ... other image attributes
+}
