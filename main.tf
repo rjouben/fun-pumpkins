9849576a16709c5c3fa54ac01402b5f38f25310a
rjouben <rjouben@fun-pumpkins.net>
2025-06-04 19:13:11 -0500

    consolidate

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    harvester = {
+      source  = "harvester/harvester"
+      version = "0.6.4"
+    }
+  }
+}
+
+provider "harvester" {
+  # Path to kubeconfig file
+  kubeconfig = var.kube_config
+
+  # Alternatively the base64 encoded contents of the kubeconfig file.
+  # CAUTION: When supplying the kubeconfig as base64 encoded string, the
+  # content will be preserved in the Terraform state files in the clear.
+  # Take appropriate measures to avoid leaking sensitive information.
+  #
+  # kubeconfig = "YXBpVmVyc2lvb...xvY2FsIgo="
+
+  kubecontext = "mycontext"
+}
+
 resource "harvester_image" "Ubuntu-Server-24.04-LTS" {
   name             = "Ubuntu-Server-24.04-LTS"
   namespace        = "harvester-public"
